f596daf5e0296eb17a1f785c25c8df556b133239
Kuret <r.littel@outlook.com>
2017-08-22 10:55:53 +0200

    Update arch install script

--- install-arch.sh
+++ install-arch.sh
@@ -4,6 +4,15 @@ if [ $(id -u) != 0 ]; then
    sudo -v
 fi
 
+# Windows <-> Linux clipboard support
+if ! type -a lemonade &> /dev/null; then
+   mkdir ~/lemonade_tmp
+   wget -P ~/lemonade_tmp/ -q https://github.com/pocke/lemonade/releases/download/v1.1.1/lemonade_linux_amd64.tar.gz
+   tar xzf ~/lemonade_tmp/lemonade_linux_amd64.tar.gz -C ~/lemonade_tmp/
+   sudo cp ~/lemonade_tmp/lemonade /usr/local/bin
+   rm -rf ~/lemonade_tmp/
+fi
+
 # Tmux
 sudo pacman -S tmux --noconfirm
 
@@ -14,7 +23,8 @@ yaourt -S ruby-neovim --noconfirm
 
 # Zsh + Antigen
 sudo pacman -S zsh --noconfirm
-yaourt -S antigen-git --noconfirm
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
+chmod -R 755 ~/.zplug
 
 # Gnu Stow
 sudo pacman -S stow --noconfirm
@@ -50,18 +60,29 @@ export $HOMEUSER=$USER # Preserve current username
 sudo -Eu postgres initdb --locale $LANG -E UTF8 -D '/var/lib/postgres/data'
 sudo -Eu postgres createuser $HOMEUSER --superuser
 
-# Ruby/Rbenv/etc
-yaourt -S rbenv --noconfirm
-yaourt -S ruby-build --noconfirm
-yaourt -S rbenv-binstubs --noconfirm
-rbenv install 2.4.0
-rbenv global 2.4.0
+# Asdf version manager
+git clone https://github.com/asdf-vm/asdf.git "${HOME}/.asdf" --branch v0.3.0
+source ~/.asdf/asdf.sh
+
+# Ruby
+asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf install ruby 2.4.0
+asdf global ruby 2.4.0
 
 zsh -c "gem install bundler"
 zsh -c "gem install foreman"
 zsh -c "gem install rails -v 5.0.1"
 
-# Elixir/Phoenix
-sudo pacman -S elixir --noconfirm
-zsh -c "mix local.hex"
-zsh -c "mix archive.install https://github.com/phoenixframework/archives/raw/master/phx_new.ez"
+# Elixir
+asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
+asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+
+asdf install erlang 20.0
+asdf global erlang 20.0
+
+asdf install elixir 1.5.0
+asdf global elixir 1.5.0
+
+zsh -c "mix local.hex --force"
+zsh -c "mix archive.install https://github.com/phoenixframework/archives/raw/master/phx_new.ez --force"
+
